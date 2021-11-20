8eaa0ef467c2f8f915aa163bd548547b80e0c960
joaquinlmartin <joaquinlmartin@gmail.com>
2021-11-20 10:39:01 +0100

    done

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((num1, num2) => num1 - num2);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] == undefined){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  }
+  
+  max() {
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.max(...this.items)
+    }
+  }
 
-  max() {}
-
-  min() {}
+  min() {
+    if(this.items.length == 0){
+      throw new Error('EmptySortedList');
+    }else{
+      return Math.min(...this.items)
+    }
+  }
 
   sum() {}
 
